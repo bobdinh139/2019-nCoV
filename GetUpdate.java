8d0c35409c7e218a483da8e604a0d2207a5a1d36
e <49532012+bobdinh139@users.noreply.github.com>
2020-02-06 19:10:08 -0500

    manually patch 4

--- GetUpdate.java
+++ GetUpdate.java
@@ -14,12 +14,12 @@ public class GetUpdate {
 	private String parser = "";
 	private String allstring="";
 	private String parse="";
-
+	private	String continent[] = {"Asia", "America","Oceania","Europe", "."};
 	public GetUpdate() throws IOException {
 		ParseEcdc() ;
 	}
-
-
+	
+	
 	private void ParseEcdc() throws IOException {
 		URL url = new URL("https://www.ecdc.europa.eu/en/geographical-distribution-2019-ncov-cases");
 		HttpURLConnection con = (HttpURLConnection) url.openConnection();
@@ -36,7 +36,7 @@ public class GetUpdate {
 		allstring =  parser.substring(parser.indexOf("Asia"));
 
 		parser = parser.substring(parser.indexOf("Asia")).replace(" and ", ", ");
-		String continent[] = {"Asia", "Europe","America","Oceania", "."};
+
 		String listOfCountries[] = new String[4];
 
 		for (int i=0; i< continent.length-1;i++) {
@@ -70,7 +70,7 @@ public class GetUpdate {
 			country.set(i,  country.get(i).replace("<p>", ""));
 			country.set(i,  country.get(i).replace("</p>", ""));
 			country.set(i,  country.get(i).replace("<>", ""));
-
+			
 			for (int j=0; j< continent.length; j++) {
 				country.set(i,country.get(i).replace(continent[j], ""));
 
@@ -80,14 +80,14 @@ public class GetUpdate {
 	}
 
 	public int getAllDeaths() throws IOException {
-		String death = allstring.substring(allstring.indexOf("Oceania"), allstring.indexOf("deaths"));
+		String death = allstring.substring(allstring.indexOf(continent[3]), allstring.indexOf("deaths"));
 		String death2 = death.substring(death.indexOf("<p>"));
 
 		return ParseDeaths(death2);
 	}
 
 	public int getChinaDeaths() throws IOException {
-		String chinadeath = allstring.substring(allstring.indexOf("Oceania"));
+		String chinadeath = allstring.substring(allstring.indexOf(continent[3]));
 		String death2 = chinadeath.substring(chinadeath.indexOf("deaths"), chinadeath.indexOf("China"));
 
 		return ParseDeaths(death2);
@@ -110,7 +110,7 @@ public class GetUpdate {
 	}
 
 	public String getDateUpdated() throws IOException {
-		String temp = parse.substring(parse.indexOf("<strong>"));
+    String temp = parse.substring(parse.indexOf("<strong>"));
 		lastUpdateDate = temp.substring(temp.indexOf("Situation"), temp.indexOf("CET")+3);
 		return lastUpdateDate;
 	}
